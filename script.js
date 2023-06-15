5e21122e403f4757a6a1c684475c6791e163d921
Bill-Bo-Baggins-Pop-OS <charlesvill@g.ucla.edu>
2023-06-14 22:14:32 -0700

    fixed bug dividing amounts by 0, the inapropriate use of equal sign before appropriate and started adding in keyboard functionality. current state of keyboard use is broken.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let regIn = false;
 let opPress = false;
 let clearElig = false;
 let newOp = false;
+const screenCharLimit = 12;
 
 let displayText = document.querySelector(".cont.screen");
 const buttons = document.querySelectorAll(".num.btn");
@@ -16,7 +17,7 @@ const cbutton = document.querySelector(".c.btn");
 
 
 buttons.forEach(button=>{
-    button.addEventListener('click',registerInput);
+    button.addEventListener('click',clickInput);
 })
 opButtons.forEach(button=>{
     button.addEventListener('click',registerOp);
@@ -26,11 +27,46 @@ equalButton.addEventListener('click',registerEqual);
 acbutton.addEventListener('click',registerAC);
 cbutton.addEventListener('click', registerC);
 
+document.addEventListener('keydown', (event) => {
+    
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    registerKeyEvent(event.key);
+  }, false);
+
+  function registerKeyEvent(key){
+    if (key === 'Shift'){
+        //do nothing
+        return;
+    }
+    else {
+        switch(key){
+            case "1","2","3","4","5","6","7","8","9","0":
+                registerInput(key);
+                break;
+            case "*", "+", "-", "/": 
+                registerOp(key);
+                break;
+            case "=": 
+                registerEqual();
+                default: console.log(`key pressed: ${key} and the type of is: ${typeof(key)}`);
+                registerInput(key);
+                //keyinputs register in display however the switch opearator completely broken. does not work. 
+        }
+    }
+    //passthrough the event.key
+    //allow shift inputs
+    //conditional sort keypresses to allow for shift presses
+    //ignore other inputs
+
+    //call other functions per matched keypresses.
+
+  }
 
 const add = (num1, num2)=>{
     return num1 + num2;
 }
- 
+
 const subtract = (num1, num2)=>{
     return num1 - num2;
 }
@@ -44,33 +80,44 @@ const divide = (num1, num2) =>{
 }
 
 function operate(operator,num1,num2){
-    switch(operator){
-        case "+":
-            return add(num1,num2);
-        case "-":
-            return subtract(num1,num2);
-        case "*":
-            return multiply(num1,num2);
-        case "/":
-            return divide(num1,num2);
-        default: console.log("something went wrong!");
+    if((operator === "*" || operator === "/") && (num1 === 0 || num2 === 0 )){
+        return 0;
     }
+    else{
+
+        switch(operator){
+            case "+":
+                return add(num1,num2);
+            case "-":
+                return subtract(num1,num2);
+            case "*":
+                return multiply(num1,num2);
+            case "/":
+                return divide(num1,num2);
+            default: console.log("something went wrong!");
+        }
+    }
+
 }
 
 function registerEqual(){
      newOp = true;
-   if (opPress || regIn){
-    opPress = false;
-    regIn = false;
-    num2 = Number(displayText.textContent);
-    result = operate(operator,num1,num2);
-    displayText.textContent = result;
-   }
-   else {
-    num1 = Number(displayText.textContent);
-    result = operate(operator,num1,num2);
-    displayText.textContent = result;
-    }
+     clearElig = true;
+     if (num1 !== undefined && operator !== undefined){
+        if (opPress || regIn){
+            opPress = false;
+            regIn = false;
+            num2 = Number(displayText.textContent);
+            result = operate(operator,num1,num2);
+            displayText.textContent = result;
+           }
+           else {
+            num1 = Number(displayText.textContent);
+            result = operate(operator,num1,num2);
+            displayText.textContent = result;
+            }
+     }
+
 }
 
 
@@ -78,12 +125,31 @@ function clearDisplay(){
     displayText.textContent = "";
 }
 
+function updateDisplay(result){
+    const updateText = toString(result);
 
-function registerInput(e){
+    if(updateText.length > 15){
+        displayText.textContent = Number(updateText).toFixed(screenCharLimit);
+
+    }
+    else {
+        displayText.textContent = updateText;
+    }
+
+}
+//refactoring input will be needed. 
+//separate function to translate click event to input
+
+function clickInput(e){
+    let num = e.target.attributes["data-btn"].value;
+    registerInput(input);
+}
+
+function registerInput(input){
   regIn = true;
-  opPress = false; 
-  newOp = false; 
-    let input = e.target.attributes["data-btn"].value;
+  opPress = false;
+  newOp = false;
+    
         if(displayText.textContent.length < 15)
         {
             const arr = Array.from(displayText.textContent);
@@ -114,16 +180,16 @@ function registerInput(e){
                         }
                     }
                 }
-                
+
         }else return
-        
+
 }
 
 function registerOp(e){
     opPress = true;
-    clearElig = true; 
-    
-    
+    clearElig = true;
+
+
     if (regIn){
         console.log("op pressed");
         regIn = false;
@@ -143,7 +209,7 @@ function registerOp(e){
             num2 = Number(displayText.textContent);
             console.log("both nums have been filled");
             result = operate(operator,num1,num2);
-            displayText.textContent = result; 
+            displayText.textContent = result;
             operator = e.target.attributes["data-btn"].value;
             // opElig = false;
         }
@@ -152,7 +218,8 @@ function registerOp(e){
     else if (newOp){
         num1 = result;
         num2 = undefined;
-        operator = e.target.attributes["data-btn"].value; 
+        result = undefined;
+        operator = e.target.attributes["data-btn"].value;
     }
 
 }
@@ -166,11 +233,11 @@ function registerAC(){
 
 function registerC(){
     //should check if the value is null or 0
-    if(displayText.textContent.length === 1){
+    if(displayText.textContent.length === 1 || result != undefined){
         return registerAC();
     }else{
-        displayText.textContent= displayText.textContent.slice(0,-1);
+        displayText.textContent = displayText.textContent.slice(0,-1);
     }
-    
+
 
 }
